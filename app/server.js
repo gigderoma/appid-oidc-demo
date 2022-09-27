0f3717f592597e79b6bbb14b29180be5b2bef890
GiG <g.gargaro@gmail.com>
2022-09-27 17:33:26 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -25,7 +25,7 @@ passport.use(new WebAppStrategy({
 	clientId: process.env.CLIENTID,
 	secret: process.env.SECRET,
 	oauthServerUrl: process.env.OAUTHSRVURL,
-	redirectUri: "http://localhost:8080/appid/callback"
+	redirectUri: "http://"+process.env.CE_APP+"."+process.env.CE_SUBDOMAIN+"."+process.env.CE_DOMAIN+"/appid/callback"
 }));
 
 // Handle Login
